2d05f0e37e496b88cf569e7d4369827bb3d9398b
Pedro Paulo <pedroabreu11@hotmail.com>
2023-08-05 19:52:29 -0300

    organizando inicio

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('cadastrar/', views.cadastrar, name='cadastrar')
+    path('logout/', views.user_logout, name='logout'),
+    path('cadastro/', views.cadastro, name='cadastro'),
 ]
